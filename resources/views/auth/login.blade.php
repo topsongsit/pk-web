22fb89fcc5c77c60a370293aa5f17ac87794ef9c
topsongsit <apple@Apples-MacBook-Pro.local>
2020-02-06 22:59:22 +0700

    [backend]-edit all , photo | [frontend]-edit profile , layout login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,50 +8,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>P.K.SaenchaiMuayThai</title>
 
-
-    <header style="font-family: 'Prompt', sans-serif;">
-        <ul class="navbar-nav">
-        <!-- Fixed navbar -->
-        <nav class="navbar navbar-expand-md navbar-dark fixed-top" style="background-color: #C30F28">        <a class="navbar-brand" href="/home">P.K.SaenchaiMuayThaiGym</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarCollapse">
-                <ul class="navbar-nav ml-auto">
-                    <li>
-                        <a class="nav-link" href="/home">หน้าหลัก</a>
-                    </li>
-                    <li>
-                        <a class="nav-link" href="/course">คอร์สเรียน</a>
-                    </li>
-                    <li>
-                        <a class="nav-link" href="/trainer">ผู้สอน</a>
-                    </li>
-                    <li>
-                        <a class="nav-link" href="/stage">สนามมวย</a>
-                    </li>
-                    <li>
-                        <a class="nav-link" href="/contact">ติดต่อเรา</a>
-                    </li>
-                    
-                    @if(\Auth::check())
-                    <li class="nav-link">
-                        <a class="">คุณ: {{ \Auth::user()->name }}</a>
-                            <a  href="/logout">logout</a>
-                        </div>
-                    </li>
-                    @else
-                    <li>
-                        <a class="nav-link" href="/register">สมัครสมาชิก</a>
-                    </li>
-                    <li>
-                        <a class="nav-link" href="/login">เข้าสู่ระบบ</a>
-                    </li>
-                    @endif
-                </ul>        
-            </div>
-        </nav>
-    </header>
     <div class="container-fluid">
         @yield('content')
     </div>
